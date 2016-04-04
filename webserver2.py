6e87bd640916dc45e89428dfbf4296a830300c32
hydroner <wangkaihuiwkh@163.com>
2016-04-04 22:56:40 +0800

    update

--- webserver2.py
+++ webserver2.py
@@ -4,17 +4,17 @@ import socket
 import StringIO
 import sys
 
+
 class WSGIServer(object):
     address_family = socket.AF_INET  #协议族AF_INET代表使用ipv4
     socket_type = socket.SOCK_STREAM  #socket类型
     request_queue_size = 1
-    
+
     def __init__(self, server_address):
         #Create a listening socket
         self.listen_socket = listen_socket = socket.socket(
             self.address_family,
-            self.socket_type
-        )
+            self.socket_type)
         #Allow to reuse the same address
         listen_socket.setsocktop(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         #bind
@@ -27,6 +27,111 @@ class WSGIServer(object):
         self.server_port = port
         #Return headers set by Web framework/Web application
         self.headers_set = []
-        
+
     def set_app(self, appliacation):
         self.application = application
+
+    def server_forever(self):
+        listen_socket = self.listen_socket
+        while True:
+            #New client connection
+            self.client_connection, client_address = listen_socket.accept()
+            #Handle one request and close the client connection.Then
+            #loop over to wait for anthor client connection.
+            self.handle_one_request()
+
+    def handle_one_request(self):
+        self.request_data = request_data = self.client_connection.recv(1024)
+        #Print formatted request a la 'curl -v'
+        print ''.join(
+            '< {line}/n'.format(line=line)
+            for line in request_data.splitlines())
+
+        self.parse_request(request_data)
+
+        #Construct environment dictionary using request data
+        env = sel.get_envion()
+
+        #It time to call our application callable and
+        #get back a result that will become HTTP reponse body
+        result = self.application(env, self.start_response)
+
+        #Construct a response and send it back to a client
+        self.finish_response(result)
+
+    def parse_request(self, text):
+        request_line = text.splitlines()[0]
+        request_line = request_line.rstrip('/r/n')
+        #Break down the request line into components
+        (self.request_method, #GET
+         self.path,           #/hello
+         self.request_version #HTTP/1.1
+        ) = request.split()
+        
+    def get_environ(self):
+        env = {}
+        #The following code snippet does not follow PEP8 conventions
+        #but it's formatted the way it is for demonstration purposes
+        #to emphasize the required variables and values.
+        #
+        #Required WSGI variables
+        env['wsgi.version'] = (1, 0)
+        env['wsgi.url_scheme'] = 'http'
+        env['wsgi.input'] = StringIO.StringIO(self.request_data)
+        env['errors'] = sys.stderr
+        env['wsgi.multithread'] = False
+        env['wsgi.multiprocess'] = False
+        env['wsgi.run_once'] = False
+        #Required CGI variables
+        env['REQUEST_METHOD'] = self.request_method #GET
+        env['PATH_INFO'] = self.path                #/hello
+        env['SERVER_NAME'] = self.server_name       #localhost
+        env['SERVER_PORT'] = str(self.server_port)  #8888
+        
+    def start_reponse(self, status, response_headers, exc_info=None):
+        #Add necessary server headers
+        server_headers = [
+            ('Date', 'Tue, 31 Mar 2015 12:54:48 GMT'),
+            ('Server', 'WSGIServer 0.2'),
+            ]
+            self.headers_set = [status, response_headers + server_headers]
+            #To adhere to WSGI specification the start_response must return
+            #a 'write' callable. We simplicity's sake we'll ignore the detail
+            #for now.
+            #return self.finish_response
+            
+    def finish_response(self, result):
+        try:
+            status, response_headers = self.headers_set
+            response = 'HTTP/1.1 {status}/r/n'.format(status=status)
+            for header in response_headers:
+                response += '{0}: {1}/r/n'.format(*header)
+            response += '/r/n'
+            for data in result:
+                response += data
+            #Print formatted response data a la 'curl -v'
+            print ''.join(
+                '>{line}/n'.format(line=line)
+                for line in response.splitlines())
+            self.client_connection.sendall(response)
+        finally:
+            self.client_connection.close()
+            
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+def make_server(server_address, application):
+    server = WSGIServer(server_address)
+    server.set_app(application)
+    return server
+
+if __name__ = '__main__':
+    if len(sys.argv) < 2:
+        sys.exit('Provide a WSGI application object as module:callable')
+    app_path = sys.argv[1]
+    module, application = app_path.split(':')
+    module = __import__(module)
+    application = getattr(module, application)
+    httpd = make_server(SERVER_ADDRESS, application)
+    print 'WSGIServer:Serving HTTp on port{port}.../n'.format(port=PORT)
+    heepd.serve_forever()
+        
+        
